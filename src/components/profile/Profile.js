64eb4b22750b677ad6141d9fdb7e6a5c2a6bcd07
Brayan Bernardo <brbernardo90@gmail.com>
2024-07-26 13:37:39 -0300

    fix profile text

--- src/components/profile/Profile.js
+++ src/components/profile/Profile.js
@@ -12,7 +12,8 @@ const Profile = () => {
             </div>
             <div className="info-container">
                 <div className="text-container">
-                    <h2>Hello, my name is Brayan Bernardo.</h2>
+                    <h1 className="hello">Hello <img width={'50px'} src = "waving-hand.svg" alt="My Happy SVG"/></h1>
+                    <h2>My name is Brayan Bernardo</h2>
                     <p> I'm a curious software engineer with a background in <b>Machine Learning Research</b>, <b>Full Stack Web Development</b>, and <b>Quality Assurance</b>. </p>
                     <p> With a strong foundation in software development and artificial intelligence, I'm passionate about using my expertise to solve real-world challenges. My career has been a diverse adventure, contributing to impactful projects in the tech industry, academia, and social movements, each enriching my journey with valuable lessons and experiences.</p>
                 </div>
